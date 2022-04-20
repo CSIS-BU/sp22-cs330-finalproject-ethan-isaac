c8e0870544fe8c75a14e6600cc712b6d32794aab
Ethan Smith <ethan@ethansmithdevelopment.com>
2022-04-20 16:03:51 -0500

    add logic for basic deck actions

--- client.py
+++ client.py
@@ -2,12 +2,27 @@
 import asyncio
 import websockets
 
-async def hello():
+# handle game logic
+async def game_loop():
+    # server address
     uri = 'ws://localhost:8765'
+    # connect to server
     async with websockets.connect(uri) as websocket:
-        name = input("What's your name?")
+        print("Lets play a guessing game! \n Guess a number between 1 and 100")
+        guess = 0
 
-        await websocket.send(name)
+        try:
+            while True:
+                # get guess from client
+                guess = int(input("Your guess: "))
+                # send guess to server
+                await websocket.send(str(guess))
+                # get response from server
+                response = await websocket.recv()
+                # print response
+                print("RESPONSE: {}".format(response))
+        except Exception as e:
+            print("Error: {}".format(e))
 
-        response = await websocket.recv()
-        print(f"<{response}")
+
+asyncio.get_event_loop().run_until_complete(game_loop())
--- server.py
+++ server.py
@@ -1,19 +1,71 @@
 # server.py
+import random
 import asyncio
 import websockets
 
+# card suits
+SUITS = ["Hearts", "Diamonds", "Clubs", "Spades"]
+
+# royal cards
+ROYAL = ["Jack", "Queen", "King", "Ace"]
+
+def get_card_suit(card):
+    return SUITS[card[0]]
+
+def get_card_value(card):
+    if card[1] == 1:
+        return ROYAL[3]
+    elif card[1] in [11, 12, 13]:
+        return ROYAL[card[1] - 11]
+    else:
+        return card[1]
+
+# print deck of cards with suits and ranks
+def print_deck(deck):
+    print("Shuffled deck of {} cards:".format(len(deck)))
+    for card in deck:
+        print(get_card_value(card), " of ",get_card_suit(card))
+
+# create card deck
+def create_fresh_deck():
+    deck = []
+    for suit in range(4):
+        for rank in range(1, 14):
+            deck.append((suit, rank))
+    return deck
+
+# shuffle deck
+def shuffle_deck(deck):
+    random.shuffle(deck)
+    return deck
+
+# create new game
+def create_new_game():
+    # create deck
+    deck = create_fresh_deck()
+    # shuffle deck
+    deck = shuffle_deck(deck)
+    # return deck
+    return deck
+
+deck = create_new_game()
+print_deck(deck)
+
 # server 
-async def run_server(websocket, path): 
-    # get websocket id
-    name = await websocket.recv()
-    print("from client: {}".format(name))
+# async def run_server(websocket, path): 
+#     # generate random number between 1 and 100
+#     answer = int(100 * (random.random())) + 1
+#     # get websocket id
+#     guess = await websocket.recv()
+#     print("Received: {}".format(guess))
+#     if(answer == int(guess)):
+#         await websocket.send("You got it!")
+#     else:
+#         await websocket.send("Try again!")
 
-    # send message to client
-    await websocket.send("Hello {}!".format(name))
 
+# start_server = websockets.serve(run_server, "localhost", 8765)
 
-start_server = websockets.serve(run_server, "localhost", 8765)
-
-# start server
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+# # start server
+# asyncio.get_event_loop().run_until_complete(start_server)
+# asyncio.get_event_loop().run_forever()

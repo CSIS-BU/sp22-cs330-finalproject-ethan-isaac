13a0df9553ec0f34546c99edb0e5622a0e34253d
Ethan Smith <ethan@ethansmithdevelopment.com>
2022-04-26 21:17:54 -0500

    add most of game logic

--- client.py
+++ client.py
@@ -1,14 +1,39 @@
 # client.py
 import socket
-from time import sleep 
 import json
+from time import sleep
+
+# card suits
+SUITS = ["Hearts", "Diamonds", "Clubs", "Spades"]
+
+# royal cards
+ROYAL = ["Jack", "Queen", "King", "Ace"]
 
 HOST = "127.0.0.1" 
-PORT = 8888
+PORT = 8088
 BUFFER_SIZE = 1024
 game_state = {}
 player = 0
 game_code = ""
+
+def get_card_suit(card):
+    return SUITS[card[0]]
+
+def get_card_value(card):
+    if card[1] == 1:
+        return ROYAL[3]
+    elif card[1] in [11, 12, 13]:
+        return ROYAL[card[1] - 11]
+    else:
+        return card[1]
+
+# print deck of cards with suits and ranks
+def print_deck(deck):
+    print("")
+    for card in deck:
+        print("     [", get_card_value(card), " of ",get_card_suit(card) ,"]")
+    
+    print("")
 
 def send_message(message, s):
         message = message.encode()
@@ -16,30 +41,37 @@ def send_message(message, s):
         data = s.recv(BUFFER_SIZE)
         return data.decode()
 
-def handle_action(action, s):
+def handle_action(action, s, game_state, player):
     # check
     if action == "c":
-        game_state = send_message(str(game_code) + " check", s)
+        return send_message(str(game_code) + " check", s)
     # bet
     elif action == "b":
         # prompt for bet amount
         bet_amount = input("Enter bet amount: ")
-        game_state = send_message(str(game_code) + " bet " + bet_amount, s)
+        while int(bet_amount) > game_state["player_" + str(player) + "_balance"]:
+            bet_amount = input("Bet amount exceeds available balance. Enter bet amount: ")
+        return send_message(str(game_code) + " bet " + bet_amount, s)
         
     # call
     elif action == "a":
-        game_state = send_message(str(game_code) + " call", s)
+        return send_message(str(game_code) + " call", s)
     # raise
     elif action == "r":
-        pass
+        # prompt for raise amount
+        raise_amount = input("Enter what you would like to raise by: ")
+        while int(raise_amount) > game_state["player_" + str(player) + "_balance"] - game_state["pending_bet"]:
+            raise_amount = input("Raise amount exceeds available balance. Enter raise amount: ")
+        return send_message(str(game_code) + " raise " + raise_amount, s)
     # fold
     elif action == "f":
-        game_state = send_message(str(game_code) + " fold", s)
+        return send_message(str(game_code) + " fold", s)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     game_state = {}
     player = 0
+    print("\033c")
     game_code = input("Welcome to Heads Up Poker! Please enter your game code, or press enter to start a new game: ")
     if game_code == "":
         game_code = send_message("new_game", s)
@@ -48,30 +80,38 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         player = 1
         game_state = json.loads(s.recv(BUFFER_SIZE).decode())
     else:
-        game_state = json.loads(send_message("join_game " + game_code + " 2", s))
+        game_state = json.loads(send_message("join_game " + game_code, s))
         player = 2
 
 
+    print("\033c")
     while True:
-        if(game_state["comment"]  and len(game_state["comment"]) > 0):
-            print(game_state["comment"])
-
-        print("Comment", game_state["comment"])
-
         turn = game_state["turn"]
-        print("Player " + str(turn) + " is up")
+        if(game_state["status"] == 1):
+            # clear terminal
+            print("\033c")
+    
+        if len(game_state['comment']) > 0:
+            print("\n\n\n" + game_state["comment"] )
+
+        print("Your hand is: ")
+        print_deck(game_state["player_" + str(player) + "_hand"])
+        print("Your balance is: " + str(game_state["player_" + str(player) + "_balance"]))
+        print("Your blind is: " + str(game_state["player_" + str(player) + "_blind"]))
+        print("Your opponent's balance is: " + str(game_state["player_" + str(3 - player) + "_balance"]))
+        print("Your opponent's blind is: " + str(game_state["player_" + str(3 - player) + "_blind"]))
+        print("The pot is: " + str(game_state["pot_balance"]))
+        # print("The cards on the table are: " + str(game_state["throw_down_cards"]))
+        print("The cards on the table are: " )
+        print_deck(game_state["throw_down_cards"])
 
         if turn == player:
-            print("Your hand is: " + str(game_state["player_" + str(player) + "_hand"]))
-            print("Your balance is: " + str(game_state["player_" + str(player) + "_balance"]))
-            print("Your blind is: " + str(game_state["player_" + str(player) + "_blind"]))
-            print("Your opponent's balance is: " + str(game_state["player_" + str(3 - player) + "_balance"]))
-            print("Your opponent's blind is: " + str(game_state["player_" + str(3 - player) + "_blind"]))
-            print("The pot is: " + str(game_state["pot_balance"]))
-            print("The cards on the table are: " + str(game_state["throw_down_cards"]))
-            print("Your turn!")
+            print("You are up")
+            # print("Your hand is: " + str(game_state["player_" + str(player) + "_hand"]))
+            
             allowed_options = ["check(c)", "bet(b)", "call(a)", "raise(r)", "fold(f)"]
             if game_state["pending_bet"] > 0:
+                print("The pending bet is: " + str(game_state["pending_bet"]))
                 allowed_options = ["call(a) opponents bet of " + str(game_state["pending_bet"]), "raise(r)", "fold(f)"]
             print("\n\n Your options are: " + str(allowed_options))
             action = input("\n\nPlease enter your action: ")
@@ -82,8 +122,9 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 while action not in ["a", "b", "c", "r", "f"]:
                     action = input(str(action) + " is not a valid option. Please enter your action: ")
 
-            handle_action(action, s)
+            game_state = json.loads(handle_action(action, s, game_state, player))
         else:
+            print("Player " + str(turn) + " is up\n")
             game_state = json.loads(s.recv(BUFFER_SIZE).decode())
 
 
--- server.py
+++ server.py
@@ -1,14 +1,12 @@
 # server.py
-from calendar import c
 import random
 import socket
-import time
 import threading
 import json
 
 # server setup
 HOST = "127.0.0.1"
-PORT = 8888  
+PORT = 8088  
 BUFFER_SIZE = 1024
 
 used_game_codes = []
@@ -59,10 +57,12 @@ def shuffle_deck(deck):
     random.shuffle(deck)
     return deck
 
-# evaluate hand
 def evaluate_hand(hand):
-    # sort hand
-    hand.sort()
+    print("Evaluating hand: ", hand)
+    print_deck(hand)
+    # check for royal flush
+    if hand[0][1] == 1 and hand[1][1] == 10 and hand[2][1] == 11 and hand[3][1] == 12 and hand[4][1] == 13:
+        return "Royal Flush"
     # check for flush
     if hand[0][0] == hand[1][0] == hand[2][0] == hand[3][0] == hand[4][0]:
         return "Flush"
@@ -71,21 +71,41 @@ def evaluate_hand(hand):
         return "Straight"
     # check for four of a kind
     if hand[0][1] == hand[1][1] == hand[2][1] == hand[3][1]:
-        return "Four of a kind"
+        return "Four of a Kind"
     # check for full house
     if hand[0][1] == hand[1][1] == hand[2][1] and hand[3][1] == hand[4][1]:
-        return "Full house"
+        return "Full House"
     # check for three of a kind
     if hand[0][1] == hand[1][1] == hand[2][1]:
-        return "Three of a kind"
+        return "Three of a Kind"
     # check for two pair
     if hand[0][1] == hand[1][1] and hand[2][1] == hand[3][1]:
-        return "Two pair"
+        return "Two Pair"
     # check for pair
     if hand[0][1] == hand[1][1]:
         return "Pair"
     # check for high card
-    return "High card"
+    return "High Card"
+
+# evaluate hand
+def evaluate_possible_hands(cards):
+    # sort hand
+    # sort hand by card value
+    cards.sort(key=lambda x: x[1])
+    possible_hands = [
+        cards[0:5],
+        cards[1:6],
+        cards[2:7],
+    ]
+    hand_result, hand_rank = "", 0
+    for hand in possible_hands:
+        result = evaluate_hand(hand)
+        print("EVAL", result)
+        rank = RANKINGS[result]
+        if(rank >= hand_rank):
+            hand_result, hand_rank = result, rank
+
+    return hand_result, hand_rank
 
 # deal cards to players
 def deal_cards(game_state):
@@ -124,13 +144,32 @@ def create_new_game(player1):
         "player_1_hand" : [],
         "player_2_hand": [],
         "player_1_blind": 2,
-        "player_2_blind": 1,
+        "player_2_blind": 2,
         "throw_down_cards": [],
         "pot_balance": 0,
         "deck": deck,
         "pending_bet": 0,
+        "last_to_bet": 1,
         "comment": "",
+        "status": 0
         }
+
+# reset game state for new round
+def reset_game_state(game_state):
+    # reset game state
+    game_state["turn"] = 2
+    game_state["player_1_hand"] = []
+    game_state["player_2_hand"] = []
+    game_state["throw_down_cards"] = []
+    game_state["pot_balance"] = 0
+    game_state["pending_bet"] = 0
+    game_state["comment"] = ""
+    game_state["state"] = 0
+    game_state["last_to_bet"] = 2
+    deck = create_fresh_deck()
+    game_state["deck"] = shuffle_deck(deck)
+    # return game state
+    return game_state
 
 def get_public_game_state(game_state, player, comment = ""):
     if player == 1:
@@ -146,7 +185,8 @@ def get_public_game_state(game_state, player, comment = ""):
             "throw_down_cards": game_state["throw_down_cards"],
             "pot_balance": game_state["pot_balance"],
             "pending_bet": game_state["pending_bet"],
-            "comment" : comment
+            "comment" : comment,
+            "status": game_state["status"]
             }
     else:
         return {
@@ -161,7 +201,8 @@ def get_public_game_state(game_state, player, comment = ""):
             "throw_down_cards": game_state["throw_down_cards"],
             "pot_balance": game_state["pot_balance"],
             "pending_bet": game_state["pending_bet"],
-            "comment" : comment
+            "comment" : comment,
+            "status": game_state["status"]
             }
 
 def send_message_to_addr(addr, message):
@@ -169,22 +210,82 @@ def send_message_to_addr(addr, message):
     conn.sendall(message.encode())
 
 def send_game_state_to_players(code, comment = ""):
+
     send_message_to_addr(games[code]["player_1"], json.dumps(get_public_game_state(games[code], 1, comment)))
     send_message_to_addr(games[code]["player_2"], json.dumps(get_public_game_state(games[code], 2, comment)))
-       
+
+def next_cards(code):
+    game_state = games[code]
+    # if there are no cards on the table deal the flop
+    if len(game_state["throw_down_cards"]) == 0:
+        # burn a card
+        game_state["deck"].pop(0)
+        # deal cards to table
+        game_state["throw_down_cards"].append(game_state["deck"].pop())
+        game_state["throw_down_cards"].append(game_state["deck"].pop())
+        game_state["throw_down_cards"].append(game_state["deck"].pop())
+        # send game state to players
+        send_game_state_to_players(code, "")
+    # if there are three cards on the table deal the turn
+    elif len(game_state["throw_down_cards"]) == 3:
+        # burn a card
+        game_state["deck"].pop(0)
+        # deal cards to table
+        game_state["throw_down_cards"].append(game_state["deck"].pop())
+        # send game state to players
+        send_game_state_to_players(code, "")
+    # if there are four cards on the table deal the river
+    elif len(game_state["throw_down_cards"]) == 4:
+        # burn a card
+        game_state["deck"].pop(0)
+        # deal cards to table
+        game_state["throw_down_cards"].append(game_state["deck"].pop())
+        # send game state to players
+        send_game_state_to_players(code, "")
+    # if there are five cards on the table end the game
+    elif len(game_state["throw_down_cards"]) == 5:
+        # evaluate possible hands
+        player_1_hand = evaluate_possible_hands(game_state["player_1_hand"] + game_state["throw_down_cards"])
+        player_2_hand = evaluate_possible_hands(game_state["player_2_hand"] + game_state["throw_down_cards"])
+        print("RESULTS", player_1_hand, player_2_hand)
+        comment = "Player 1: " + player_1_hand[0] + " || Player 2: " + player_2_hand[0]
+        reset_game_state(games[code])
+        deal_cards(games[code])
+        if(player_1_hand[1] > player_2_hand[1]):
+            game_state["player_1_balance"] += game_state["pot_balance"]
+            winner = "Player 1 Wins Hand"
+        elif(player_1_hand[1] < player_2_hand[1]):
+            game_state["player_2_balance"] += game_state["pot_balance"]
+            winner = "Player 2 Wins Hand"
+        else:
+            game_state["player_1_balance"] += int(game_state["pot_balance"]) / 2
+            games[code]["player_2_balance"] += int(game_state["pot_balance"]) / 2
+            winner = "Tie Hand"
+        
+        # print player balances
+        print("Player 1 Balance: " + str(game_state["player_1_balance"]))
+        print("Player 2 Balance: " + str(game_state["player_2_balance"]))
+
+        if(game_state["player_1_balance"] <= 0):
+            winner = "Player 2 Wins with " + str(game_state["player_1_balance"]) + " Chips"
+        elif(game_state["player_2_balance"] <= 0):
+            winner = "Player 1 Wins with " + str(game_state["player_2_balance"]) + " Chips"
+        
+        games[code] = game_state
+        comment += "\n" + winner
+        send_game_state_to_players(code, comment)
 
 def handle_action(message, player):
     if(message == "new_game"):
         # generate unused game code
-        code = random.randint(0, 9999)
+        code = random.randint(1000, 9999)
         while code in games:
-            code = random.randint(0, 9999)
-        
-        games[code] = create_new_game(player)
+            code = random.randint(1000, 9999)
+
+        games[str(code)] = create_new_game(player)
         return str(code) # use current timestamp as game id
     elif message[0:9] == "join_game":
-        code = message[10:len(message) - 2]
-        print("CODE", code)
+        code = message[10:]
         games[code]["player_2"] = player
         # deal cards
         games[code] = deal_cards(games[code])
@@ -196,39 +297,58 @@ def handle_action(message, player):
     code = ""
     if(len(message) > 4):
         code = message[:4]
-        msg = message[4:]
+        msg = message[5:]
 
-    print("CODE", code, "MSG", msg)
+    print("CODE", code, "MSG |" + msg + "|")
 
+    
     comment = ""
     # preemptively change turn
     current_turn = games[code]["turn"]
+    games[code]["status"] = 1
     games[code]["turn"] = 1 if current_turn == 2 else 2
     if msg == "check":
         comment = "Player " + str(current_turn) + " checked"
     elif "bet" in msg:
         amount = int(msg[4:])
-        games[code]["player_" + str(current_turn) + "_balance"] -= amount
-        games[code]["pot_balance"] += amount
-        games[code]["pending_bet"] = int(msg[4:])
+        # games[code]["player_" + str(current_turn) + "_balance"] -= amount
+        # games[code]["pot_balance"] += amount
+        games[code]["pending_bet"] = amount
+        games[code]["last_to_bet"] = current_turn
         comment = "Player " + str(current_turn) + " bet " + str(amount)
     elif msg == "call":
         amount = games[code]["pending_bet"]
-        games[code]["player_" + str(current_turn) + "_balance"] -= amount
-        games[code]["pot_balance"] += amount
+        games[code]["player_1_balance"] -= amount
+        games[code]["player_2_balance"] -= amount
+        games[code]["pot_balance"] += amount + amount
         games[code]["pending_bet"] = 0
         comment = "Player " + str(current_turn) + " called " + str(amount)
-    elif msg == "raise":
-        amount = int(msg[4:])
-        games[code]["player_" + str(current_turn) + "_balance"] -= amount
-        games[code]["pot_balance"] += amount
-        games[code]["pending_bet"] = int(msg[4:])
+    elif "raise" in msg:
+        amount = int(msg[6:])
+        # games[code]["player_" + str(current_turn) + "_balance"] -= games[code]["pot_balance"] + amount
+        # games[code]["pot_balance"] += amount
+        games[code]["pending_bet"] = games[code]["pending_bet"] + amount
         comment = "Player " + str(current_turn) + " raised " + str(amount)
+        games[code]["last_to_bet"] = current_turn
+        print("RAISED TO", games[code]["pending_bet"])
     elif msg == "fold":
-        comment = "Player " + str(current_turn) + " folded "
+        print("Adding to player " + str(3 - current_turn) + " hand amount " + str(games[code]["pot_balance"]))
+        games[code]["player_" + str(3 - current_turn) + "_balance"] += games[code]["pot_balance"]
+        # reset game state
+        reset_game_state(games[code])
+        deal_cards(games[code])  
+        send_game_state_to_players(code, comment)
+        return 
 
-
-    send_game_state_to_players(code, comment)
+    if games[code]["pending_bet"] > 0:
+        if(current_turn != games[code]["last_to_bet"]):
+            next_cards(code)   
+        else:
+            send_game_state_to_players(code, comment)
+    elif(current_turn != 2):
+        next_cards(code)
+    else:
+        send_game_state_to_players(code, comment)
 
     return ""
 
@@ -236,17 +356,17 @@ def handle_current_connection(conn, addr):
     while True:
         # receive data
         data = conn.recv(BUFFER_SIZE).decode()
-        if not data:
-            continue
-        elif data == "close":
+        # if not data:
+        #     continue
+        if data == "close":
             print("Connection from {} closed".format(addr))
             # remove connection from selector
             connections.pop(addr)
         else:
             # process data
-            print("Received data: {} from {}".format(data, addr))
+            print("Received data from {}: {}".format(addr, data))
             res = handle_action(data, addr)
-            if len(res) > 0:
+            if res and len(res) > 0:
                 send_message_to_addr(addr, res)
 
 
@@ -257,6 +377,9 @@ sock.bind((HOST, PORT))
 # listen for incoming connections
 sock.listen(10)
 
+# clear terminal
+print("\033c")
+print("Listening on port " + str(PORT))
 while True:
     # accept connection
     conn, addr = sock.accept()
